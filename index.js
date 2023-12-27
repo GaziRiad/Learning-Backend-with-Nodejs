814c7a9d7665930b74bad1ab311935f7f5caab89
GaziRiad <ouhibimustapha36@gmail.com>
2023-12-27 15:48:42 +0100

    Reading and writing files asynchronously

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log(textIn);
+// Blocking synchronousy way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log(textIn);
+
+// Non-Blocking asynchronouse way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.error(err.message);
+  console.log(data1);
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    if (err) return console.error(err.message);
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      if (err) return console.error(err.message);
+      console.log(data3);
+      fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, "utf-8", (err) => {
+        if (err) return console.error(err.message);
+      });
+    });
+  });
+});
+
+console.log("will read file.");
